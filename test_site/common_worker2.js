6ca6782793c9065fe90e77395c7bad11de007ca4
Ben Last <ben@benlast.com>
2016-04-20 14:40:56 -0700

    Added tests for multiple event handlers

--- test_site/common_worker2.js
+++ test_site/common_worker2.js
@@ -1,12 +1,15 @@
 /**
- * Created by benlast on 2016-04-12.
+ * Second event handlers for key events, so that we can verify what
+ * happens as we handle events.
  */
 'use strict';
+
+let WORKER2 = 'worker2';
 
 self.addEventListener(
     'activate',
     function(event) {
-        console.log(WORKER + ' activate event');
+        console.log(WORKER2 + ' activate event');
         event.waitUntil(
             self.clients.claim()
         )
@@ -16,28 +19,19 @@ self.addEventListener(
 self.addEventListener(
     'install',
     function(event) {
-        console.log(WORKER + ' install event');
+        console.log(WORKER2 + ' install event');
         event.waitUntil(
             self.skipWaiting()
         );
     }
 );
 
-let MOBIFY_PNG_RE = /(.*)\/mobify\d.png/;
-
 self.addEventListener(
     'fetch',
     function(event) {
         console.log(
-            WORKER + ' fetch event for ' + event.request.url
+            WORKER2 + ' fetch event for ' + event.request.url
         );
-        let match = MOBIFY_PNG_RE.exec(event.request.url);
-        if (match) {
-            console.log(WORKER + ' intercepted fetch');
-            event.respondWith(
-                fetch(match[1] + '/mobify.png')
-            );
-        }
         // Let the event fall through so that the resource is fetched
         // as normal.
     }
@@ -47,7 +41,7 @@ self.addEventListener(
     'push',
     function(event) {
         console.log(
-            WORKER + ' push event'
+            WORKER2 + ' push event'
         )
     }
 );
